6034dd9bbafaf44c04197c2235c8faf50ba5b05a
Sharmistha Das <69760738+sharmistha412@users.noreply.github.com>
2021-04-02 22:58:26 +0530

    question fragment added

--- app/src/main/java/com/example/firebaselogin/fitit/Home_page.java
+++ app/src/main/java/com/example/firebaselogin/fitit/Home_page.java
@@ -2,14 +2,18 @@ package com.example.firebaselogin.fitit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Home_page extends AppCompatActivity {
+public class Home_page extends AppCompatActivity implements View.OnClickListener {
+
+    public CardView c1,c2,c3,c4,c5,c6,c7,c8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,21 @@ public class Home_page extends AppCompatActivity {
 
 //      getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
 
+        c1=(CardView)findViewById(R.id.card1);
+        c2=(CardView)findViewById(R.id.card2);
+        c3=(CardView)findViewById(R.id.card3);
+        c4=(CardView)findViewById(R.id.card4);
+        c5=(CardView)findViewById(R.id.card5);
+        c6=(CardView)findViewById(R.id.card6);
+        c7=(CardView)findViewById(R.id.card7);
+        c8=(CardView)findViewById(R.id.card8);
+
+        c1.setOnClickListener(this);
+        c2.setOnClickListener(this);
+
+
     }
+    //Bottom navBar
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -44,4 +62,10 @@ public class Home_page extends AppCompatActivity {
 
         }
     };
+
+    @Override
+    public void onClick(View v) {
+        Fragment selectedFragment= new QuestionForItemFragment();
+         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+    }
 }
